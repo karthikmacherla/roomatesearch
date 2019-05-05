e5d0bcba8ba530ce859cb350bf2743dcbc6b48b2
KarthikMacherla <karthik.macherla123@gmail.com>
2019-05-05 13:55:25 -0400

    Updated with templates

--- roomatesearch/settings.py
+++ roomatesearch/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'roomatesearch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'main.CustomUser'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
